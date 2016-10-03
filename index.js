d06d3dc2f610a9b2593fd4ba59e8b999542fd9dd
Willow Bellemore <willow.alex@bellemore.ca>
2016-10-03 16:21:22 +0200

    Fix syntax for auth token/sids

--- index.js
+++ index.js
@@ -2,9 +2,12 @@
 
 var bodyParser = require('body-parser')
 var express = require('express')
-var twilio = require('twilio')(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN)
 var path = require('path')
 var call = require('./call.js')
+
+var twilio = require('twilio')
+var client = twilio(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN)
+var twiml = twilio.TwimlResponse();
 
 let app = express()
 app.use(express.static('public'))
@@ -17,7 +20,7 @@ app.get('/', function(req, res) {
 app.post('/voice', (req, res) => {
 
   let fileUrl = 'https://dl.dropboxusercontent.com/u/162794740/hacktoberfest/2spooky4me.mp3'
-  let twiml = new twilio.TwimlResponse()
+  let twiml = new client.TwimlResponse()
   twiml.play(fileUrl)
 
   res.header('Content-Type', 'text/xml')
