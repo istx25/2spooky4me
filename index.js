945f803528c283d004a6154234ca87d63e7979d9
brennanMKE <brennan@smallsharptools.com>
2016-10-03 13:03:51 -0700

    fixes Twilio integration

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ var express = require('express')
 var path = require('path')
 var call = require('./call.js')
 
-var client = require('twilio')(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN)
+var twilio = require('twilio');
+var client = new twilio.RestClient(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN);
 
 let app = express()
 app.use(express.static('public'))
@@ -16,14 +17,12 @@ app.get('/', function(req, res) {
 })
 
 app.post('/voice', (req, res) => {
-
   let fileUrl = 'https://dl.dropboxusercontent.com/u/162794740/hacktoberfest/2spooky4me.mp3'
-  let twiml = client.TwimlResponse()
+  let twiml = twilio.TwimlResponse()
   twiml.play(fileUrl)
 
   res.header('Content-Type', 'text/xml')
   res.send(twiml.toString())
-
 })
 
 app.post('/form', (req, res) => {
