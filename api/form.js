b669f71e6d4abf93cbfacea8f625049ff6667786
Luna Graysen <luna.g@protonmail.com>
2019-10-21 18:33:26 -0400

    Run through prettier

--- api/form.js
+++ api/form.js
@@ -1,7 +1,11 @@
-var twilio = require("twilio");
+const twilio = require("twilio");
 
 module.exports = (req, res) => {
-  var client = new twilio.RestClient(process.env.TWILIO_SID, process.env.TWILIO_AUTH_TOKEN);
+  var client = new twilio.RestClient(
+    process.env.TWILIO_SID,
+    process.env.TWILIO_AUTH_TOKEN
+  );
+
   let url = "https://" + req.headers["x-now-deployment-url"] + "/api/voice";
   client.makeCall({
     to: req.body.recipient,
--- api/voice.js
+++ api/voice.js
@@ -1,9 +1,10 @@
 const VoiceResponse = require("twilio").twiml.VoiceResponse;
 
 module.exports = (req, res) => {
-    let fileUrl = "https://api.twilio.com/cowbell.mp3";
-    let response = new VoiceResponse();
-    response.play(fileUrl);
-    res.writeHead(200, { "Content-Type": "text/xml" });
-    res.end(response.toString());
+  // https://dl.dropboxusercontent.com/s/j7fiqn8hi0akbzz/musical-skeletons.mp3
+  let fileUrl = "https://api.twilio.com/cowbell.mp3";
+  let response = new VoiceResponse();
+  response.play(fileUrl);
+  res.writeHead(200, { "Content-Type": "text/xml" });
+  res.end(response.toString());
 };
