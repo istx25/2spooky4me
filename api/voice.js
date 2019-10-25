d2c08e6d3ae0fe2331eb81072ff0338df9b15bfe
Luna Graysen <luna.g@protonmail.com>
2019-10-25 19:29:56 -0400

    Fix audio response

--- api/voice.js
+++ api/voice.js
@@ -3,8 +3,8 @@ const VoiceResponse = require("twilio").twiml.VoiceResponse;
 module.exports = (req, res) => {
   // https://dl.dropboxusercontent.com/s/j7fiqn8hi0akbzz/musical-skeletons.mp3
   let fileUrl = "https://api.twilio.com/cowbell.mp3";
-  let response = new VoiceResponse();
-  response.play(fileUrl);
-  res.writeHead(200, { "Content-Type": "text/xml" });
-  res.end(response.toString());
+  let audioResponse = new VoiceResponse();
+  audioResponse.play(fileUrl);
+  res.set("Content-Type", "text/xml");
+  res.end(audioResponse.toString());
 };
