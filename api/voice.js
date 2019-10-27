8730bb116fd29f948d597133eff4a1a5462a3343
Luna Graysen <luna.g@protonmail.com>
2019-10-26 20:33:18 -0400

    Change let to const

--- api/voice.js
+++ api/voice.js
@@ -1,9 +1,9 @@
-const VoiceResponse = require("twilio").twiml.VoiceResponse;
+const VoiceResponse = require('twilio').twiml.VoiceResponse;
 
 module.exports = (req, res) => {
   // https://dl.dropboxusercontent.com/s/j7fiqn8hi0akbzz/musical-skeletons.mp3
   let fileUrl = "https://api.twilio.com/cowbell.mp3";
-  let audioResponse = new VoiceResponse();
+  const response = new VoiceResponse();
   audioResponse.play(fileUrl);
   res.status(200);
   res.set("Content-Type", "text/xml");
