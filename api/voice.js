69f5f127af4c666534e58bef4e3b5394816eb9ae
Luna Graysen <luna.g@protonmail.com>
2019-10-28 09:51:35 -0400

    Update voice.js

--- api/voice.js
+++ api/voice.js
@@ -1,9 +1,9 @@
-const VoiceResponse = require('twilio').twiml.VoiceResponse;
+const twilio = require("twilio");
 
 module.exports = (req, res) => {
   // https://dl.dropboxusercontent.com/s/j7fiqn8hi0akbzz/musical-skeletons.mp3
   let fileUrl = "https://api.twilio.com/cowbell.mp3";
-  const audioResponse = new VoiceResponse();
+  const audioResponse = new twilio.twiml.VoiceResponse();
   audioResponse.play(fileUrl);
   res.status(200);
   res.set("Content-Type", "text/xml");
